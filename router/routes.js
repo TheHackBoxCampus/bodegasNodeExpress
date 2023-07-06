05a9db551c2802a0dd4c47cfca2cdde4bfa7ba79
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-07-06 09:36:05 -0500

    feat: :sparkles: Validaciones endpoint Inventarios
    Se hicieron validaciones,para el tipo de dato,  data recolectada, datos incompletos, sin valor.

--- router/routes.js
+++ router/routes.js
@@ -31,7 +31,6 @@ router.get('/productosOrden', (req, res) => {
              )
          ) AS Bodega
     FROM productos AS p
-    GROUP BY Producto
     HAVING Total IS NOT NULL 
     ORDER BY Total DESC`;
     connection.query(query, (err, progress) => err ? res.send(err) : res.json(progress));
@@ -45,28 +44,57 @@ router.post('/inventarios', (req, res) => {
     connection.query(gettingPattern, (err, progress) => {
         if(err) res.send(err)
         else {
-            let exist = false; 
-            for(let property in progress) {
-                if(progress[property].producto == data_values[0] && progress[property].bodega == data_values[1]){
-                    exist = !exist
-                    break; 
-                }
-            }
-            if(exist) {
-                let updateCount = /* sql */ `UPDATE inventarios SET cantidad = cantidad + ? WHERE id_producto = ? && id_bodega = ?`;
-                connection.query(updateCount, [data_values[2], data_values[0], data_values[1]], (err, progress) => {
-                    err ? res.send(err) : res.json({"status": 200, "message": "Se actualizo la cantidad del patron seleccionado"})
-                });
+            if(Object.entries(data).length <= 0 || data_values.length == 0) {
+                res.status(500).send({"message": "Por favor envie data"});
             }else {
-                let insertPattern = /* sql */ `INSERT INTO inventarios (id_producto, id_bodega, cantidad) VALUES (?, ?, ?)`;
-                connection.query(insertPattern, [...data_values], (err, progress) => {
-                    err ? res.send(err) : res.json({"status": 200, "message": "Se inserto el nuevo patron"}); 
-                })
+                if(data_values.length <= 2 || Object.entries(data).length <= 2){
+                    res.status(500).send({"message": "La data esta incompleta"})
+                }else {
+                    let typeofincorrect = false;
+                    for(let x = 0; x < data_values.length; x++){
+                        if(typeof data_values[x] != "number") typeofincorrect = true;
+                    }
+                    if(typeofincorrect) {
+                        res.status(500).send({"message": "Verifique el tipo de dato de los items"}); 
+                    }else {
+                        let exist = false; 
+                        for(let property in progress) {
+                            if(progress[property].producto == data["producto"] && progress[property].bodega == data["bodega"]){
+                                exist = !exist
+                                break; 
+                            }
+                        }
+                        if(exist) {
+                            if(data["id"]){
+                                res.status(500).send({"message": "El patron existe, coloca el patron y la cantidad, sin la ID"});
+                            }else {
+                                let updateCount = /* sql */ `UPDATE inventarios SET cantidad = cantidad + ? WHERE id_producto = ? && id_bodega = ?`;
+                                connection.query(updateCount, [data_values[2], data_values[0], data_values[1]], (err, progress) => {
+                                    err ? res.send(err) : res.json({"status": 200, "message": "Se actualizo la cantidad del patron seleccionado"})
+                                });
+                            }
+                        }else {
+                            if(data["id"] != undefined){
+                                let insertPattern = /* sql */ `INSERT INTO inventarios (id, id_producto, id_bodega, cantidad) VALUES (?, ?, ?, ?)`;
+                                connection.query(insertPattern, [...data_values], (err, progress) => {
+                                if(err){
+                                    if(err.errno == 1062) {
+                                        res.json({"message": "La id que envio ya existe ingrese una nueva!!"})
+                                    }
+                                }else {
+                                    res.json({"status": 200, "message": "Se inserto el nuevo patron"})
+                                }
+                                }); 
+                            }else res.status(500).send({"message": "El patron no existe, coloca el ID del campo"}); 
+                        }
+                    }
+                }
             }
         }
     })
-    
 })
+
+
 
 // post data from the warehouses table 
 router.post('/bodegas', (req, res) => {
